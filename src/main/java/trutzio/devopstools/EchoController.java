cf9675ed3004e8d3ee04b174a0d531326d863570
Christian Trutz <info@trutz-software-consulting.de>
2024-04-17 13:29:13 +0200

    fix: kein warten im Thread mehr

--- src/main/java/trutzio/devopstools/EchoController.java
+++ src/main/java/trutzio/devopstools/EchoController.java
@@ -5,16 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 
-
 @RestController
 public class EchoController {
 
-    @SuppressWarnings("static-access")
     @GetMapping("/")
     @WithSpan(value = "test span")
     public String echo() throws InterruptedException {
-        Thread.currentThread().sleep(100);
         return "OK";
     }
-    
+
 }
