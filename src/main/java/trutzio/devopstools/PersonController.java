c33b58a22e31df41961b9c5f89299b7a23a6a7b7
Christian Trutz <info@trutz-software-consulting.de>
2025-04-06 19:03:33 +0200

    refactor: replace Tempo with Jaeger in Docker Compose and update Grafana datasource configuration

--- src/main/java/trutzio/devopstools/PersonController.java
+++ src/main/java/trutzio/devopstools/PersonController.java
@@ -2,8 +2,6 @@ package trutzio.devopstools;
 
 import java.util.List;
 
-import javax.swing.text.html.parser.Entity;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
